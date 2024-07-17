3d61dc522f8fedb52576ab5bab47eedb1d6904f2
xfy <i@rua.plus>
2024-07-17 13:49:31 +0800

    feat(errors): implement convert core error

--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,9 @@
-use std::io;
-
 #[derive(thiserror::Error, Debug)]
-pub enum AppError {}
+pub enum AppError {
+    #[error("venus core error {0}")]
+    VenusCore(#[from] venus_core::error::VenusError),
+    #[error("venus config error {0}")]
+    VenusConfig(#[from] venus_core::config::error::ConfigError),
+}
 
 pub type AppResult<T, E = AppError> = Result<T, E>;
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use dotenvy::dotenv;
 use error::AppResult;
 use tracing::info;
 use utils::init_logger;
+use venus_core::Venus;
 
 mod consts;
 mod core;
@@ -12,6 +13,9 @@ mod utils;
 async fn main() -> AppResult<()> {
     dotenv().ok();
     init_logger();
+
+    let mut venus = Venus::new()?;
+    venus.config.reload_rua()?;
 
     info!("Hello, world!");
     Ok(())
