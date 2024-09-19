eeab1b0bb86ad676134177176a0f82270d09d72a
xfy <xfyxfy@proton.me>
2024-09-19 16:27:43 +0800

    refactor(logging): update trace middleware

--- venus/src/middlewares/mod.rs
+++ venus/src/middlewares/mod.rs
@@ -50,21 +50,21 @@ pub fn logging_route(router: Router) -> Router {
         info_span!("HTTP", method = ?req.method(), host, uri = ?req.uri(), ua)
     };
 
-    router.layer(
-        TraceLayer::new_for_http()
-            .make_span_with(make_span)
-            .on_request(|_req: &Request<_>, _span: &Span| {})
-            .on_response(|res: &Response, latency: Duration, _span: &Span| {
-                info!("{}", format_latency(latency, res.status()));
-            })
-            .on_body_chunk(|_chunk: &Bytes, _latency: Duration, _span: &Span| {})
-            .on_eos(|_trailers: Option<&HeaderMap>, _stream_duration: Duration, _span: &Span| {})
-            .on_failure(
-                |error: ServerErrorsFailureClass, latency: Duration, _span: &Span| {
-                    error!("{}", format_latency(latency, error));
-                },
-            ),
-    )
+    let trace_layer = TraceLayer::new_for_http()
+        .make_span_with(make_span)
+        .on_request(|_req: &Request<_>, _span: &Span| {})
+        .on_response(|res: &Response, latency: Duration, _span: &Span| {
+            info!("{}", format_latency(latency, res.status()));
+        })
+        .on_body_chunk(|_chunk: &Bytes, _latency: Duration, _span: &Span| {})
+        .on_eos(|_trailers: Option<&HeaderMap>, _stream_duration: Duration, _span: &Span| {})
+        .on_failure(
+            |error: ServerErrorsFailureClass, latency: Duration, _span: &Span| {
+                error!("{}", format_latency(latency, error));
+            },
+        );
+
+    router.layer(trace_layer)
 }
 
 /// Format request latency and status message
