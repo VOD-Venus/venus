d9e9c7d442de5ad3ee4ed4ab080933e114215273
xfy <xfyxfy@proton.me>
2024-09-29 08:41:39 +0800

    refactor(ui): update errors
    update button callback

--- venus-ui/src/components/button.rs
+++ venus-ui/src/components/button.rs
@@ -6,15 +6,10 @@ use crate::clsx;
 
 /// A parameterized incrementing button
 #[component]
-pub fn Button(
-    children: Children,
-    #[prop(optional)] class: Cow<'static, str>,
-    #[prop(optional)] on_click: Option<Box<dyn Fn()>>,
-) -> impl IntoView {
+pub fn Button(children: Children, #[prop(optional)] class: Cow<'static, str>) -> impl IntoView {
     view! {
-        <button
-            class=clsx!(
-                "bg-white border border-transparent hover:border-gray-200",
+        <button class=clsx!(
+            "bg-white border border-transparent hover:border-gray-200",
           "outline-none hover:bg-gray-50 focus:ring-4 dark:border-transparent",
           "focus:ring-cyan-200 font-medium rounded-lg text-sm",
           "px-5 py-2.5 dark:bg-gray-800 dark:text-white ",
@@ -26,14 +21,6 @@ pub fn Button(
           "dark:disabled:bg-gray-700 dark:disabled:text-gray-300",
           "disabled:dark:hover:border-transparent",
           class
-            )
-            on:click=move |_| {
-                if let Some(c) = on_click.as_ref() {
-                    c()
-                }
-            }
-        >
-            {children().nodes.into_iter().collect::<Vec<_>>()}
-        </button>
+        )>{children().nodes.into_iter().collect::<Vec<_>>()}</button>
     }
 }
--- venus-ui/src/pages/home.rs
+++ venus-ui/src/pages/home.rs
@@ -1,4 +1,4 @@
-use crate::components::button::Button;
+use crate::components::{button::Button, errors::ErrorsView};
 use leptos::*;
 
 /// Default Home Page
@@ -8,32 +8,17 @@ pub fn Home() -> impl IntoView {
 
     view! {
         <ErrorBoundary fallback=|errors| {
-            view! {
-                <h1>"Uh oh! Something went wrong!"</h1>
-
-                <p>"Errors: "</p>
-                // Render a list of errors as strings - good for development purposes
-                <ul>
-                    {move || {
-                        errors
-                            .get()
-                            .into_iter()
-                            .map(|(_, e)| view! { <li>{e.to_string()}</li> })
-                            .collect_view()
-                    }}
-
-                </ul>
-            }
+            view! { <ErrorsView errors=errors /> }
         }>
 
             <div>
                 <h1>"Welcome to Leptos"</h1>
 
                 <div class="p-2">
-                    <Button on_click=Box::new(|| { set_count(count() + 1) }) class="mr-2".into()>
+                    <Button on:click=move |_| set_count(count() + 1) class="mr-2".into()>
                         {count}
                     </Button>
-                   <Button>{count}</Button>
+                // <Button>{count}</Button>
                 </div>
             </div>
         </ErrorBoundary>
