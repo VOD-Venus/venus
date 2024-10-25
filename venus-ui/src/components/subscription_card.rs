b6b95a696b67e96c9fdc65be90638bb9888d19df
xfy <xfyxfy@proton.me>
2024-10-25 16:14:29 +0800

    feat(ui): subscription form

--- venus-ui/src/components/subscription_card.rs
+++ venus-ui/src/components/subscription_card.rs
@@ -8,8 +8,18 @@ pub fn SubscriptionCard() -> impl IntoView {
         <dialog id="add_modal" class="modal">
             <div class="modal-box">
                 <h3 class="text-lg font-bold">Subscription</h3>
-                <p class="py-4">Press ESC key or click the button below to close</p>
+
+                <div class="py-4 flex flex-col gap-4">
+                    <label class="input input-bordered flex items-center gap-2">
+                        Name <input type="text" class="grow" placeholder="rua" />
+                    </label>
+                    <label class="input input-bordered flex items-center gap-2">
+                        URL <input type="text" class="grow" placeholder="https://rua.rua" />
+                    </label>
+                </div>
+
                 <div class="modal-action">
+                    <button class="btn btn-primary">Confirm</button>
                     <form method="dialog">
                         <button class="btn">Close</button>
                     </form>
