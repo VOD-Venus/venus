562ee52c82d21d97134257122c3b0bb9d7f6e4fd
xfy <xfyxfy@proton.me>
2024-11-01 10:52:57 +0800

    feat(ui): add mobile sidebar

--- venus-ui/src/components/sidebar.rs
+++ venus-ui/src/components/sidebar.rs
@@ -29,7 +29,7 @@ const NAVI: [Navi; 5] = [
 ];
 
 #[component]
-pub fn Sidebar() -> impl IntoView {
+fn SidebarContent() -> impl IntoView {
     let location = use_location();
 
     let children = move |n: Navi| {
@@ -48,11 +48,43 @@ pub fn Sidebar() -> impl IntoView {
 
     view! {
         <>
+            // logo
+            <div class="flex justify-center w-full">
+                <img alt="venus" src="public/venus.svg" class="object-contain w-28 h-28" />
+            </div>
+
+            // nav
+            <div class="flex flex-col justify-between h-full felx-1">
+                <div class="flex flex-col my-4">
+                    <ul class="my-4 menu bg-base-200 rounded-box">
+                        <For each=move || NAVI key=|n| n.path children=children />
+                    </ul>
+                </div>
+            </div>
+        </>
+    }
+}
+
+#[component]
+pub fn Sidebar() -> impl IntoView {
+    view! {
+        <>
+            <nav class="sm:flex flex-col w-56 max-w-xs px-5 py-6 bg-gray-100 dark:bg-rua-gray-900 hidden">
+                <SidebarContent />
+            </nav>
+        </>
+    }
+}
+
+#[component]
+pub fn SidebarMobile() -> impl IntoView {
+    view! {
+        <div>
             // mobile drawer button
-            <div class="z-10 sm:hidden">
+            <div class="z-10 sm:hidden relative">
                 <input id="my-drawer" type="checkbox" class="drawer-toggle" />
                 // <!-- Page content here -->
-                <label for="my-drawer" class="btn btn-primary drawer-button fixed right-4">
+                <label for="my-drawer" class="btn btn-primary drawer-button">
                     Open drawer
                 </label>
                 <div class="drawer-side">
@@ -61,34 +93,11 @@ pub fn Sidebar() -> impl IntoView {
                         aria-label="close sidebar"
                         class="drawer-overlay"
                     ></label>
-                    <ul class="menu bg-base-200 text-base-content min-h-full w-80 p-4">
-                        // <!-- Sidebar content here -->
-                        <li>
-                            <a>Sidebar Item 1</a>
-                        </li>
-                        <li>
-                            <a>Sidebar Item 2</a>
-                        </li>
+                    <ul class="menu bg-base-200 text-base-content min-h-full w-72 p-4">
+                        <SidebarContent />
                     </ul>
                 </div>
             </div>
-
-            <nav class="sm:flex flex-col w-56 max-w-xs px-5 py-6 bg-gray-100 dark:bg-rua-gray-900 hidden">
-
-                // logo
-                <div class="flex justify-center w-full">
-                    <img alt="venus" src="public/venus.svg" class="object-contain w-28 h-28" />
-                </div>
-
-                // nav
-                <div class="flex flex-col justify-between h-full felx-1">
-                    <div class="flex flex-col my-4">
-                        <ul class="my-4 menu bg-base-200 rounded-box">
-                            <For each=move || NAVI key=|n| n.path children=children />
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        </>
+        </div>
     }
 }
--- venus-ui/src/layout.rs
+++ venus-ui/src/layout.rs
@@ -1,6 +1,9 @@
 use leptos::*;
 
-use crate::components::{errors::ErrorsView, sidebar::Sidebar};
+use crate::components::{
+    errors::ErrorsView,
+    sidebar::{Sidebar, SidebarMobile},
+};
 
 #[component]
 pub fn Layout(children: Children) -> impl IntoView {
@@ -12,6 +15,7 @@ pub fn Layout(children: Children) -> impl IntoView {
                 <Sidebar />
 
                 <div class="flex-1 h-full p-8 overflow-auto">
+                    <SidebarMobile />
                     {children().nodes.into_iter().collect::<Vec<_>>()}
                 </div>
             </main>
