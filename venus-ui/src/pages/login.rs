db95d91bddd0df27a726ca6d1ac341fadf4f7bf0
xfy <xfyxfy@proton.me>
2024-11-14 10:53:23 +0800

    feat(ui): add login form

--- venus-ui/src/pages/login.rs
+++ venus-ui/src/pages/login.rs
@@ -1,6 +1,103 @@
+use ev::Event;
 use leptos::*;
+
+#[derive(Debug, Clone)]
+struct LoginForm {
+    pub username: String,
+    pub password: String,
+}
 
 #[component]
 pub fn Login() -> impl IntoView {
-    view! { login }
+    let (form, set_form) = create_signal(LoginForm {
+        username: "".into(),
+        password: "".into(),
+    });
+
+    enum FormTarget {
+        Username,
+        Password,
+    }
+    let handle_change = move |target: FormTarget| {
+        use FormTarget::*;
+        move |e: Event| {
+            let value = event_target_value(&e);
+            match target {
+                Username => set_form.update(|f| f.username = value),
+                Password => set_form.update(|f| f.password = value),
+            }
+        }
+    };
+
+    view! {
+        <div class="hero bg-base-200 min-h-screen">
+            <div class="hero-content flex-col lg:flex-row-reverse">
+                <div class="text-center lg:text-left">
+                    <h1 class="text-5xl font-bold">Login now!</h1>
+                    <p class="py-6">
+                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                        quasi. In deleniti eaque aut repudiandae et a id nisi.
+                    </p>
+                </div>
+                <div class="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                    <form class="card-body">
+                        <div class="form-control">
+                            <span class="label-text mb-2">Username</span>
+                            <label class="input input-bordered flex items-center gap-2">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 16 16"
+                                    fill="currentColor"
+                                    class="h-4 w-4 opacity-70"
+                                >
+                                    <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
+                                </svg>
+                                <input
+                                    type="text"
+                                    class="grow"
+                                    prop:value=move || form().username
+                                    placeholder="Username"
+                                    required
+                                    on:change=handle_change(FormTarget::Username)
+                                />
+                            </label>
+                        </div>
+                        <div class="form-control">
+                            <span class="label-text mb-2">Password</span>
+                            <label class="input input-bordered flex items-center gap-2">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 16 16"
+                                    fill="currentColor"
+                                    class="h-4 w-4 opacity-70"
+                                >
+                                    <path
+                                        fill-rule="evenodd"
+                                        d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                                        clip-rule="evenodd"
+                                    />
+                                </svg>
+                                <input
+                                    type="password"
+                                    class="grow"
+                                    prop:value=move || form().password
+                                    placeholder="Password"
+                                    required
+                                    on:change=handle_change(FormTarget::Password)
+                                />
+                            </label>
+                            <label class="label">
+                                <a href="#" class="label-text-alt link link-hover">
+                                    Forgot password?
+                                </a>
+                            </label>
+                        </div>
+                        <div class="form-control mt-6">
+                            <button class="btn btn-primary">Login</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    }
 }
