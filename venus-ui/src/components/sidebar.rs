cb6687ad8df8780210da3e6dfff9ceb71f899546
xfy <i@rua.plus>
2024-12-13 17:27:24 +0800

    feat(ui): sidebar close animation

--- venus-ui/src/components/sidebar.rs
+++ venus-ui/src/components/sidebar.rs
@@ -1,13 +1,17 @@
+use std::time::Duration;
+
 use leptos::prelude::*;
 use leptos_router::hooks::use_location;
 
 use crate::hooks::use_global_ui;
 
+/// Navigation item
 struct Navi {
     pub name: &'static str,
     pub path: &'static str,
     pub icon: &'static str,
 }
+/// Navigation items
 const NAVI: [Navi; 5] = [
     Navi {
         name: "Proxies",
@@ -36,23 +40,61 @@ const NAVI: [Navi; 5] = [
     },
 ];
 
+/// Sidebar content
+///
+/// is_mobile: whether the sidebar is in mobile mode
+///
+/// If is mobile sidebar, the width of the sidebar is 100%, the sidebar will cannot be closed.
 #[component]
-fn SidebarContent() -> impl IntoView {
+fn SidebarContent(is_mobile: bool) -> impl IntoView {
     let location = use_location();
     let ui = use_global_ui();
 
     let children = move |n: Navi| {
         let ui = use_global_ui();
 
+        // Hide tab name when sidebar is not open
+        let (hide_name, set_hide_name) = signal(!ui.sidebar_open.get());
+        Effect::new(move |_| {
+            if is_mobile {
+                return;
+            }
+            if ui.sidebar_open.get() {
+                set_timeout(
+                    move || {
+                        set_hide_name.set(false);
+                    },
+                    Duration::from_millis(10),
+                );
+            } else {
+                set_timeout(
+                    move || {
+                        set_hide_name.set(true);
+                    },
+                    Duration::from_millis(290),
+                );
+            }
+        });
+
         view! {
             <li>
                 <a
                     href=n.path
-                    class="mb-2 flex items-center transition-all duration-300"
+                    class="mb-2 flex items-center transition-all duration-300 overflow-hidden h-9 w-full"
+                    class=("sm:w-full", move || ui.sidebar_open.get())
                     class=("btn-active", move || n.path == location.pathname.get())
+                    style:width=move || {
+                        if is_mobile {
+                            "100%"
+                        } else if ui.sidebar_open.get() {
+                            ""
+                        } else {
+                            "calc(2rem + 16px)"
+                        }
+                    }
                 >
-                    <span class=format!("{}", n.icon)></span>
-                    <span class=("hidden", move || !ui.sidebar_open.get())>{n.name}</span>
+                    <span class=format!("{} min-w-[1rem]", n.icon)></span>
+                    <span class=("hidden", move || !is_mobile && hide_name())>{n.name}</span>
                 </a>
             </li>
         }
@@ -61,7 +103,7 @@ fn SidebarContent() -> impl IntoView {
     view! {
         <>
             // logo
-            <div class="flex justify-center w-full">
+            <div class="flex justify-center w-full" class=("p-2", move || !ui.sidebar_open.get())>
                 <img
                     alt="venus"
                     src="public/venus.svg"
@@ -77,9 +119,10 @@ fn SidebarContent() -> impl IntoView {
                     </ul>
                 </div>
 
+                // toggle sidebar button
                 <div>
                     <button
-                        class="btn btn-ghost"
+                        class="btn btn-ghost hidden sm:block"
                         on:click=move |_| ui.sidebar_open.set(!ui.sidebar_open.get())
                     >
                         <span class="icon-[solar--square-double-alt-arrow-left-bold-duotone]"></span>
@@ -97,10 +140,13 @@ pub fn Sidebar() -> impl IntoView {
     view! {
         <>
             <nav
-                class="sm:flex flex-col max-w-xs px-5 py-6 bg-gray-100 dark:bg-rua-gray-900 hidden transition-all duration-300"
+                class="sm:flex flex-col max-w-xs py-6 bg-gray-100 dark:bg-rua-gray-900 hidden transition-all duration-300"
                 class=("w-56", move || ui.sidebar_open.get())
+                class=("px-5", move || ui.sidebar_open.get())
+                class=("w-20", move || !ui.sidebar_open.get())
+                class=("px-3", move || !ui.sidebar_open.get())
             >
-                <SidebarContent />
+                <SidebarContent is_mobile=false />
             </nav>
         </>
     }
@@ -127,7 +173,7 @@ pub fn SidebarMobile() -> impl IntoView {
                                 class="drawer-overlay"
                             ></label>
                             <ul class="menu bg-base-200 text-base-content min-h-full w-72 p-4">
-                                <SidebarContent />
+                                <SidebarContent is_mobile=true />
                             </ul>
                         </div>
                     </div>
